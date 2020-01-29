3ff1b3fc843a1e83ad8d01a41ae8b141b40605c0
xeroblood <xeroblood@owntheday.io>
2020-01-28 19:54:53 -0500

    Update install scripts

--- lusion_install.sh
+++ lusion_install.sh
@@ -3,33 +3,24 @@
 # Install Node 12.x
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 sudo apt-get install -y nodejs
-#sudo apt-get install gcc g++ make
-sudo apt autoremove --silent
-
+sudo apt-get install -y git
+#sudo apt autoremove --silent
 
 # Install Lusion App
 cd ~
-echo -e "\e[96mCloning repo...\e[90m"
+echo "LusionMagic: Cloning Repo..."
 if git clone --depth=1 https://github.com/robsecord/LusionMagic.git; then
-    echo -e "\e[92mCloning repo done!\e[0m"
+    echo "LusionMagic: Cloning repo done!"
 else
-    echo -e "\e[91mUnable to clone repo."
+    echo "LusionMagic: Unable to clone repo."
     exit;
 fi
 
 cd ~/LusionMagic/ || exit
-echo -e "\e[96mInstalling dependencies...\e[90m"
+echo "LusionMagic: Installing Lusion Dependencies..."
 if npm install ; then
-    echo -e "\e[92mDependencies installed!\e[0m"
+    echo "LusionMagic: Dependencies installed!"
 else
-    echo -e "\e[91mUnable to install dependencies."
-    exit;
-fi
-
-echo -e "\e[Building App...\e[90m"
-if npm run build ; then
-    echo -e "\e[App built successfully!\e[0m"
-else
-    echo -e "\e[91mUnable to build app."
+    echo "LusionMagic: Unable to install dependencies."
     exit;
 fi
--- lusion_start.sh
+++ lusion_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+cd ~/LusionMagic/
 npm run start
