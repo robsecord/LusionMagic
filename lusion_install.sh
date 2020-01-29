3ca6a466ed565bac70bb87d081fdeb9f63bafdcd
xeroblood <xeroblood@owntheday.io>
2020-01-28 21:21:05 -0500

    Update installer

--- lusion_install.sh
+++ lusion_install.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 # Install Node 12.x
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y git
+#curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+#sudo apt-get install -y nodejs
+wget https://nodejs.org/dist/latest-v12.x/node-v12.14.1-linux-armv7l.tar.gz
+tar -xzf node-v12.14.1-linux-armv7l.tar.gz
+sudo cp -R node-v12.14.1-linux-armv7l/* /usr/local/
+
+# Install GIT
+sudo apt install -y git
+
+# Install PM2
+sudo npm install -y -g pm2
+
+# Cleanup
+rm -rf node-v*
 #sudo apt autoremove --silent
 
 # Install Lusion App
