0ee46015afb3c33f448befcb69179019d1f61f5d
xeroblood <xeroblood@owntheday.io>
2020-02-01 23:17:04 -0500

    Update install script

--- lusion_install.sh
+++ lusion_install.sh
@@ -9,26 +9,35 @@ APP_VERSION=${APP_VERSION_STR:1:6}
 NODE_VERSION="12.14.1"
 NODE_VERSION_MAJOR=${NODE_VERSION:0:2}
 
-echo " "
+spacing() {
+    echo " "
+    echo " "
+}
+
+spacing
 echo "======= Lusion-Installer ======="
 echo "${APP_NAME}: v${APP_VERSION}"
 echo "Node: v${NODE_VERSION}"
 echo " "
 
-#echo "Lusion-Installer: Updating System Sources.."
+spacing
+echo "Lusion-Installer: Updating System Sources.."
 sudo apt update
 sudo apt-get update
 
+spacing
 echo "Lusion-Installer: Upgrading System Dependencies.."
 sudo apt-get upgrade -y 2>&1
-sudo apt-get dist-upgrade 2>&1
+sudo apt-get dist-upgrade -y 2>&1
 
 # Install Xserver, LXDE-gui, lightdm and other nescessary things
+spacing
 echo "Lusion-Installer: Installing extras.."
 sudo apt-get install raspberrypi-ui-mods firefox-esr build-essentials -y
 #  libgles2, libgl-mesa-dri, libglew-dev, libpng, libjpeg
 
 # Install Node 12.x
+spacing
 echo "Lusion-Installer: Downloading NodeJS v${NODE_VERSION}.."
 wget "https://nodejs.org/dist/latest-v${NODE_VERSION_MAJOR}.x/node-v${NODE_VERSION}-linux-armv7l.tar.gz"
 echo "Lusion-Installer: Extracting NodeJS.."
@@ -38,18 +47,22 @@ nodeFolder="node-v${NODE_VERSION}-linux-armv7l"
 sudo cp -R ${nodeFolder}/* /usr/local/
 
 # Install Electron
+#spacing
 #echo "Lusion-Installer: Installing Electron.."
 #npm install -g electron --unsafe-perm=true --allow-root --arch=armv7l --platform=linux
 
 # Install GIT
+#spacing
 #echo "Lusion-Installer: Installing GIT.."
 #sudo apt-get install -y git
 
 # Install PM2
+#spacing
 #echo "Lusion-Installer: Installing PM2.."
 #sudo npm install -y -g pm2
 
 # Install Lusion App
+#spacing
 #cd ~
 #echo "Lusion-Installer: Cloning Repo..."
 #if git clone --depth=1 "https://github.com/${APP_REPO}/${APP_NAME}.git"; then
@@ -65,6 +78,7 @@ unzip "${APP_NAME}-linux-armv7l-${APP_VERSION}.zip"
 
 
 #cd ~/LusionMagic/ || exit
+#spacing
 #echo "Lusion-Installer: Installing Dependencies..."
 #if npm install ; then
 #    echo "Lusion-Installer: Dependencies installed!"
@@ -74,10 +88,12 @@ unzip "${APP_NAME}-linux-armv7l-${APP_VERSION}.zip"
 #fi
 
 # Cleanup
+spacing
 echo "Lusion-Installer: Cleaning up after install.."
 rm -rf node-v*
 rm -rf "${APP_NAME}-linux-armv7l-${APP_VERSION}.zip"
-sudo apt autoremove --silent
+sudo apt autoremove --silent -y
 
+spacing
 echo "======= Lusion-Installer Complete! ======="
 echo " "
---
+++ lusion_start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Starting LusionMagic..."
+cd ~/LusionMagic-linux-armv7l/
+./LusionMagic
+
